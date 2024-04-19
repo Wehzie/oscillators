fb6adf4eaf204fd727630b50d48eb9b67be18eff
Rafael Tappe Maestro <r.tm@outlook.de>
2024-04-19 22:18:25 +0200

    adjust axes limits on oscillator plot

--- src/oscillators/target.py
+++ src/oscillators/target.py
@@ -71,10 +71,15 @@ class OscillatorGrid:
     def animate(self, frame: int, num_frames: int, fig: figure.Figure, lines: List[plt.Line2D]) -> List:
         """return the signal at a given frame index"""
         animation_phase_shift = (frame / num_frames) * 2 * np.pi
+        ax_index = 0
         for osc, line in zip(self.oscillators, lines):
             shift_amount = int(animation_phase_shift / (2 * np.pi) * len(osc.signal))
             shifted_signal = np.roll(osc.signal, -shift_amount)
             line.set_data(osc.time, shifted_signal)
+            fig.axes[ax_index].set_xlim(osc.time.min(), osc.time.max())  # set x limits
+            y_pad = 0.05
+            fig.axes[ax_index].set_ylim(shifted_signal.min() - y_pad, shifted_signal.max() + y_pad)
+            ax_index += 1
         # TODO: x and y limits
         fig.canvas.draw()
         return lines
